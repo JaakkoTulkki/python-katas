404fb7ebc7df4e9d0a893cd49fb7ea220d502d61
Jaakko Tulkki <jaakko.tulkki@sainsburys.co.uk>
2017-03-24 08:55:27 +0000

    filtering data

--- datastuff/spread.py
+++ datastuff/spread.py
@@ -1,2 +1,22 @@
-def spread(min_t, max_t):
-    return max_t - min_t
+class Weather:
+    def __init__(self, filename):
+        self.filename = filename
+        self.data = []
+
+    def spread(self, min_t, max_t):
+        return max_t - min_t
+
+    def open_file(self):
+        for row in open(self.filename, 'r'):
+            self.data.append(row)
+        self.data = [datum for datum in self.data if datum != '\n']
+        self.data = [datum.replace("\n", "").strip().split(" ") for datum in self.data]
+        data = []
+        for lst in self.data:
+            row = [datum for datum in lst if datum != ""]
+            data.append(row)
+        self.data = data
+        return self.data
+
+
+
--- datastuff/test_data.py
+++ datastuff/test_data.py
@@ -1,14 +1,22 @@
 import unittest
 
-from datastuff.spread import spread
+from datastuff.spread import Weather
 
 class TestDataStuff(unittest.TestCase):
+
+    def setUp(self):
+        self.weather = Weather('./weather.dat')
 
     def test_calculates_spread_correctly(self):
         min_t = 81
         max_t = 91
 
         expected_spread = 10
-        actual_spread = spread(min_t, max_t)
+        actual_spread = self.weather.spread(min_t, max_t)
 
         self.assertEqual(expected_spread, actual_spread)
+
+    def test_opens_file_correctly(self):
+        data = self.weather.open_file()
+        self.assertTrue(isinstance(data, list), data)
+        self.assertTrue(data)
